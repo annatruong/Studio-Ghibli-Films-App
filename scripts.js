2c98b30b8f8f43388fd52b8c7f66b3b4e833b270
annatruong <annatruong321@hotmail.com>
2021-09-01 13:48:52 +0100

    Added image and text

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
+const app = document.getElementById('main');
+
+const logo = document.createElement('img');
+logo.src = 'img/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
 fetch('https://ghibliapi.herokuapp.com/films')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     const randomFilm = data[Math.floor(Math.random()*data.length)];
-    console.log(randomFilm.title);
+    //console.log(randomFilm.title);
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Which Studio Ghibli movie should I watch tonight?';
+    container.append(h1);
   })
   .catch((err) => {
     console.log(err);
